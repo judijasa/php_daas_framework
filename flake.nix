cd2022735ef6e5ca36e4ef7b3981711265722272
Juan Jaramillo <judijasa@gmail.com>
2026-08-31 13:26:01 -0500

    Replace PROJECT_NAME in ShellHook: simox -> php_daas_framework

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
 
             # Inherit nix shell env in tmux (doesn't include PS1)
             # Requires `set -g default-command ...` in .tmux.conf
-            PROJECT_NAME="simox"
+            PROJECT_NAME="php_daas_framework"
             alias tmux="command tmux -L \$PROJECT_NAME new-session -A -s \$PROJECT_NAME"
           '';
         };
